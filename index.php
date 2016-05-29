21891805b7828d548e328f537b004eac635d19db
Gustavo Adolfo Muñoz Reyes <Gustavo Adolfo Muoz Reyes>
2016-05-29 15:16:34 -0500

    maquetaion de las dos primeras secciones de la pagina

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Popurri</title>
+	<link rel="stylesheet" href="css/ed-grid.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="ed-container total">
+		<div class="ed-item none tablet-30 web-30 no-padding">
+			<img src="img/cabezote_logo.png" alt="" class="total">
+		</div>
+		<div class="ed-item base-100 movil-100 tablet-70 web-70 no-padding fondo_gris">
+			<nav class="menu">
+				<ul>
+					<li><a href=""><i></i>Inicio</a></li>
+					<li><a href=""><i></i>Simposio</a></li>
+					<li><a href=""><i></i>Conferencias</a></li>
+					<li><a href=""><i></i>Programación</a></li>
+					<li><a href=""><i></i>Inscripción</a></li>
+					<li><a href=""><i></i>Sede</a></li>
+					<li><a href=""><i></i>Contacto</a></li>
+				</ul>
+			</nav>
+			<img src="img/sliderinicial.jpg" alt="" class="total">
+		</div>
+		<div class="ed-item base-100 movil-100 tablet-70 web-70 no-padding fondo_gris">
+			<article class="contenido">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui distinctio asperiores minus rerum, possimus cupiditate ducimus atque at excepturi nihil ab vitae, voluptates molestiae voluptatem, laborum itaque aspernatur porro odio Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui distinctio asperiores minus rerum, possimus cupiditate ducimus atque at excepturi nihil ab vitae, voluptates molestiae voluptatem, laborum itaque aspernatur porro odio</p>
+				<a href=""><img src="img/boton_leermas.png" alt=""></a>
+			</article>
+		</div>
+		<div class="ed-item base-100 movil-100 tablet-30 web-30 no-padding">
+			<a href=""><img src="img/ponencias_imagen_boton.png" alt="" class="total"></a>
+		</div>
+	</div>
+</body>
+</html>
