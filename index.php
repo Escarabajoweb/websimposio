f54ebb7e9939a39f0ff36379c266c05a8b0ee753
Gustavo Adolfo Muñoz Reyes <Gustavo Adolfo Muoz Reyes>
2016-05-29 16:51:25 -0500

    maquetacion de la parte inferiro de la pagina hasta donde estan las señoras

--- index.php
+++ index.php
@@ -34,6 +34,65 @@
 		<div class="ed-item base-100 movil-100 tablet-30 web-30 no-padding">
 			<a href=""><img src="img/ponencias_imagen_boton.png" alt="" class="total"></a>
 		</div>
+		<div class="ed-item base-100 movil-100 tablet-70 web-70 no-padding">
+			<div class="ed-container total">
+				<div class="ed-item base-100 movil-100 tablet-50 web-50 fondo">
+					<div class="j-separator-40 total"></div>
+					<h2 class="editorial"><span>01</span> Editorial</h2>
+					<h1 class="titulos">Titulo</h1>
+					<h1 class="titulos">Ejemplo XXXXX</h1>
+					<article class="noticia">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat fuga atque ducimus, explicabo fugit autem est laboriosam quia ratione culpa officiis quisquam obcaecati asperiores magnam, tempore porro recusandae, nobis consequatur.Nisi consequatur alias, iusto ullam cupiditate corporis rem ipsam voluptates doloribus beatae inventore provident laborum voluptatem doloremque quod eum corrupti, maxime, praesentium. Mollitia, dolorum iure numquam, esse natus voluptas debitis.doloremque quod eum corrupti, maxime, praesentium. Mollitia, dolorum iure numquam, esse natus</p>
+					</article>
+					<a href=""><img src="img/boton_leermas.png" alt=""></a>
+					<div class="j-separator-60 total"></div>
+				</div>
+				<div class="ed-item base-100 movil-100 tablet-50 web-50 fondo"> 
+					<div class="j-separator-40 total"></div>
+					<h2 class="editorial"><span>01</span> Editorial</h2>
+					<div class="borde">
+						<h1 class="titulos">Titulo</h1>
+						<h1 class="titulos">Ejemplo XXXXX</h1>
+						<article class="noticia">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat fuga atque ducimus, explicabo fugit autem est laboriosam quia ratione culpa officiis quisquam obcaecati asperiores magnam, tempore porro recusandae, nobis consequatur.Nisi consequatur alias, iusto ullam cupiditate corporis rem ipsam voluptates doloribus beatae inventore provident laborum voluptatem doloremque quod eum corrupti, maxime, praesentium. Mollitia, dolorum iure numquam, esse natus voluptas debitis.</p>
+						</article>
+						<a href=""><img src="img/boton_leermas.png" alt=""></a>
+						<div class="j-separator-60 total"></div>
+					</div>
+				</div>
+			</div>
+			<div class="ed-container total fondo_gris"> 
+				<div class="j-separator-60 total"></div>
+				<div class="contenedor"> 
+					<div class="ed-container total">
+						<h1 class="total">Conferencistas</h1>
+						<div class="ed-item base-100 movil-100 tablet-50 web-50">
+							<div class="total fondo_azul">
+								<p>Christine Delory - Momberger <br> Francia</p>
+							</div>
+							<div class="total p-relativa"> 
+								<img src="img/1.jpg" alt="">
+								<a href="" class="p-absoluta posicion_boton_perfil">Ver Perfil+</a>
+							</div>
+						</div> 
+						<div class="ed-item base-100 movil-100 tablet-50 web-50">
+							<div class="total fondo_azul">
+								<p>Christine Delory - Momberger <br> Francia</p>
+							</div>
+							<div class="total p-relativa"> 
+								<img src="img/1.jpg" alt="">
+								<a href="" class="p-absoluta posicion_boton_perfil">Ver Perfil+</a>
+							</div>
+						</div> 
+					</div>
+				</div>
+				<div class="j-separator-60 total"></div>
+			</div>
+
+		</div>
+		<div class="ed-item base-100 movil-100 tablet-30 web-30 no-padding">
+			<img src="img/redes.jpg" alt="">
+		</div>
 	</div>
 </body>
 </html>
